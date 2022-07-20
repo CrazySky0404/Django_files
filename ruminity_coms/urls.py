f1dc8efd19d043c4ab797e9dd510cc16234fa1a8
MaritaBaro <crazysky700@gmail.com>
2022-07-20 14:07:54 +0300

    Topic.html + subtopic.html.

--- ruminity_coms/urls.py
+++ ruminity_coms/urls.py
@@ -10,4 +10,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     # Сторінка, що відображає всі теми.
     path('topics/', views.topics, name='topics'),
+    # Сторінка, що відображає всі підтеми вибраної теми.
+    path('topics/<int:topic_id>/', views.subtopics, name='subtopics'),
+    # Сторінка, що відображає всі записи до вибраної підтеми.
+    path('topic/<int:subtopic_id>/', views.subtopic, name='subtopic'),
 ]
--- ruminity_coms/views.py
+++ ruminity_coms/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .models import Topic
+from .models import Topic, Subtopic
 
 
 def index(request):
@@ -13,3 +13,19 @@ def topics(request):
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
     return render(request, 'ruminity_coms/topics.html', context)
+
+
+def subtopics(request, topic_id):
+    """Показати всі підтеми до вибраної теми."""
+    topic = Topic.objects.get(id=topic_id)
+    subtopics = topic.subtopic_set.order_by('-date_added')
+    context = {'topic': topic, 'subtopics': subtopics}
+    return render(request, 'ruminity_coms/topic.html', context)
+
+
+def subtopic(request, subtopic_id):
+    """Показати вибрану підтему з усіма записами."""
+    subtopic = Subtopic.objects.get(id=subtopic_id)
+    entries = subtopic.entry_set.order_by('-date_added')
+    context = {'subtopic': subtopic, 'entries': entries}
+    return render(request, 'ruminity_coms/subtopic.html', context)
