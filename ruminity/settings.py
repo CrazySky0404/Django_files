3903e8f863b4e69bfaa5a536c1f5d35084ff3220
MaritaBaro <crazysky700@gmail.com>
2022-07-28 09:20:43 +0300

    Головний дизайн(жовта плашка зверху).

--- ruminity/settings.py
+++ ruminity/settings.py
@@ -35,6 +35,9 @@ INSTALLED_APPS = [
     'ruminity_coms',
     'users',
 
+    # Сторонні застосунки.
+    'bootstrap4',
+
     #Уставні застосунки django.
     'django.contrib.admin',
     'django.contrib.auth',
@@ -126,6 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    'rumi/static/',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
