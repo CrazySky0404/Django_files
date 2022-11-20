34d821727aa767de9178f98ef189a3938d7dba38
MaritaBaro <crazysky700@gmail.com>
2022-11-20 15:46:45 +0200

    Add desing profile

--- ruminity_coms/forms.py
+++ ruminity_coms/forms.py
@@ -29,51 +29,22 @@ class EntryForm(forms.ModelForm):
         widgets = {'text': forms.Textarea(attrs={'cols': 80})}
 
 
-def check_name(text):
-    if text[0].lower() != 'k':
-        print(f"TTTTTTTTTTTTTTTTTTTTTTTT{text}")
-        raise forms.ValidationError('Name should start with k')
-
-
-
 class PublicationForm(forms.ModelForm):
-    # def clean(self):
-    #     # Get the user submitted names from the cleaned_data dictionary
-    #     cleaned_data = super().clean()
-    #     text = cleaned_data.get("text")
-    #
-    #     if text[0].lower() != text[0].lower():
-    #         # If not, raise an error
-    #         raise ValidationError("The first letters of the names do not match")
-    #
-    #     return cleaned_data
-
     class Meta:
         model = Publication
         fields = ['text', 'description']
-        #labels = {'text':'Назва твору', 'description':'Текст твору'}
-        #text = forms.CharField()
-        #description = forms.CharField(widget=forms.Textarea(attrs={'max_length': 30}))
-        #text = forms.CharField(validators=[check_name])
+        labels = {'text':'Назва твору', 'description':'Текст твору'}
         text = forms.CharField()
-        # description = forms.CharField(widget=forms.Textarea(attrs={'max_length': 30}))
 
     def clean(self):
-        # data from the form is fetched using super function
         super(PublicationForm, self).clean()
         #text = self.cleaned_data.get('text')
         description = self.cleaned_data.get('description')
 
-        # conditions to be met for the username length
         if len(description) > 10000:
             self._errors['description'] = self.error_class([
                 f'Максимальна кількість символів: 10 000. Ваша кількість символів: {len(description)}'])
 
-        # if len(text) < 10:
-        #     self._errors['text'] = self.error_class([
-        #         'Post Should Contain a minimum of 10 characters'])
-
-        # return any errors if found
         return self.cleaned_data
 
 
